c68880ad34308c80483ee54b46a29e7773aaff69
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-09-16 13:47:31 +0200

    add function done

---
+++ CalculatorClassLibrary/Calculator.cs
@@ -0,0 +1,19 @@
+﻿namespace CalculatorClassLibrary
+{
+    /// <summary>
+    /// A calculator. It calculates things
+    /// </summary>
+    public static class Calculator
+    {
+        /// <summary>
+        /// Takes two numbers, does some magic shit to it and returns one number that may or may not be the two numbers added together
+        /// </summary>
+        /// <param name="x">first number</param>
+        /// <param name="y">second number</param>
+        /// <returns></returns>
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ CalculatorClassLibraryTests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClassLibrary.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            int result = Calculator.Add(10, 10);
+            Assert.AreEqual(20, result);
+        }
+    }
+}
